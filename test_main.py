be08c3b077a96917810316edd64f159a5bd32054
Lovkesh <lovkesh126116@gmail.com>
2025-07-30 17:00:45 +0530

    Final 30-Jul-Closing

--- test_main.py
+++ test_main.py
@@ -1,5 +1,8 @@
-from main import add
+from main import app
 
-def test_add():
-    assert add(2, 3) == 5
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Hello from your CI/CD pipeline deployed on Render!" in response.data
 
